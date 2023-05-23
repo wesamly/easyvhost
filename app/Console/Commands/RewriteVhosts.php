c5beb70bfe136d8db48b819e8a5f1140af2533be
Wesam Alalem <wesam@wesam.ly>
2023-05-23 13:13:08 +0200

    update: add RewriteVhosts command

---
+++ app/Console/Commands/RewriteVhosts.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\HostUpdated;
+use App\Models\Host;
+use Illuminate\Console\Command;
+
+class RewriteVhosts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vhosts:rewrite';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $host = Host::first();
+        HostUpdated::dispatch($host);
+        
+        return 0;
+    }
+}
